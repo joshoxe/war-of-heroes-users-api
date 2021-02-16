b05e46a3309ddee285aa6ff83a90b57735a65ebe
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-02-16 10:23:58 +0000

    Rename API

---
+++ WarOfHeroesAPI/FakeDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarOfHeroesAPI
+{
+    // Interim database before implementing Entity Framework
+    public class FakeDatabase
+    {
+        public List<User> Users { get; set; }
+
+        public FakeDatabase()
+        {
+            Users = new List<User>();
+            Users.Add(new User("test", "test"));
+        }
+    }
+}
---
+++ WarOfHeroesAPI/Validation/LoginValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace WarOfHeroesAPI.Validation
+{
+    public class LoginValidator : AbstractValidator<User>
+    {
+        public LoginValidator()
+        {
+            RuleFor(user => user.Username).NotNull().NotEmpty();
+            RuleFor(user => user.Password).NotNull().NotEmpty();
+        }
+    }
+}
