8bc73520bdc49de5b179f9449e68acd72aceedd6
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-02-23 15:16:49 +0000

    Remove old code

--- WarOfHeroesAPI/Controllers/UsersController.cs
+++ WarOfHeroesAPI/Controllers/UsersController.cs
@@ -18,31 +18,11 @@ namespace WarOfHeroesAPI.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        private readonly ILogger<UsersController> _logger;
-        private readonly AbstractValidator<User> _validator;
-        private readonly FakeDatabase _database;
-
-        public UsersController(ILogger<UsersController> logger, AbstractValidator<User> validator, FakeDatabase database)
+        [HttpGet]
+        public IActionResult Get()
         {
-            _logger = logger;
-            _validator = validator;
-            _database = database;
-        }
-
-        // GET api/<UsersController>/5
-        [HttpPost("login")]
-        public IActionResult Login([FromBody] object body)
-        {
-            var userFromBody = JsonConvert.DeserializeObject<User>(body.ToString());
-            var validationResult = _validator.Validate(userFromBody);
-
-            if (!validationResult.IsValid)
-            {
-                _logger.LogWarning("Invalid login request {body}", body);
-                return BadRequest("Invalid login request");
-            }
-
             return Ok();
         }
     }
 }
+
