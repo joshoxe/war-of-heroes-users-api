9d091dab18778d8804c137d0be6b7966a6a759ea
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-05-14 12:08:58 +0100

    add debug log

--- WarOfHeroesAPI/Data/UserRepository.cs
+++ WarOfHeroesAPI/Data/UserRepository.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
 using WarOfHeroesUsersAPI.Data.Entities;
 
 namespace WarOfHeroesUsersAPI.Data
@@ -8,10 +11,12 @@ namespace WarOfHeroesUsersAPI.Data
     public class UserRepository : IUserRepository
     {
         private readonly UserContext _userContext;
+        private readonly ILogger<UserContext> _logger;
 
-        public UserRepository(UserContext userContext)
+        public UserRepository(UserContext userContext, ILogger<UserContext> logger)
         {
             _userContext = userContext;
+            _logger = logger;
         }
 
         public IEnumerable<User> GetAllUsers()
@@ -22,8 +27,10 @@ namespace WarOfHeroesUsersAPI.Data
 
         public User GetUserByGoogleId(string googleId)
         {
-            return _userContext.Users.Include(u => u.UserHeroInventories)
-                .SingleOrDefault(u => u.GoogleId == googleId);
+            var userByGoogleId = _userContext.Users.Include(u => u.UserHeroInventories)
+                .Select(u => u.GoogleId);
+            _logger.LogDebug(String.Join(" ", userByGoogleId.Select(u => u)));
+           return _userContext.Users.Include(u => u.UserHeroInventories).FirstOrDefault<User>(u => googleId == u.GoogleId);
         }
 
         public void AddNewUser(User user)
