64083faa2714a5b56ad199917d6714c52d0674bb
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-05-14 09:22:37 +0100

    Add DB to pipeline

--- WarOfHeroesAPI/Deployment/main.tf
+++ WarOfHeroesAPI/Deployment/main.tf
@@ -10,18 +10,9 @@ terraform {
 backend "azurerm" {
     resource_group_name   = "tfstate"
     storage_account_name  = "tfstate28596"
-    container_name        = "tfstate"
+    container_name        = "tfstate-users"
     key                   = "tf.tfstate"
   }
-}
-
-variable "username" {
-  type        = string
-  sensitive = true
-}
-variable "password" {
-  type        = string
-  sensitive = true
 }
 
 provider "azurerm" {
@@ -29,14 +20,10 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "warOfHeroes"
+  name     = "warOfHeroesUsers"
   location = "UK South"
 }
 
-resource "azurerm_resource_group" "db_rg" {
-  name     = "warOfHeroesDBs"
-  location = "UK South"
-}
 
 resource "azurerm_app_service_plan" "app_service_plan" {
     name                = "warOfHeroes-appservice"
@@ -58,53 +45,29 @@ resource "azurerm_app_service" "users_app_service" {
     site_config {
         windows_fx_version = "DOTNETCORE|3.1"
     }
-}
-
-resource "azurerm_app_service" "heroes_app_service" {
-    name                = "warOfHeroesHeroes"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
-    https_only = true
-    site_config {
-        windows_fx_version = "DOTNETCORE|3.1"
+    connection_string {
+      name = "UserDb"
+      type = "SQLServer"
+      value = "Server=tcp:${azurerm_sql_server.users_db_server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.users_db.name};Persist Security Info=False;User ID=${azurerm_sql_server.users_db_server.administrator_login};Password=${azurerm_sql_server.users_db_server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
     }
 }
-  
-resource "azurerm_app_service" "frontend_app_service" {
-  name                = "warOfHeroes"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
-  https_only = true
-  site_config {
-      windows_fx_version = "DOTNETCORE|3.1"
-      default_documents = ["index.html"]
-  }
-}
 
-resource "azurerm_sql_server" "user_db" {
-  name                         = "UsersDb"
-  resource_group_name          = azurerm_resource_group.db_rg.name
-  location                     = azurerm_resource_group.db_rg.location
+
+resource "azurerm_sql_server" "users_db_server" {
+  name                         = "josh-users-server-123"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = "East US"
   version                      = "12.0"
-  administrator_login          = var.username
-  administrator_login_password = var.password
+  administrator_login          = var.DB_USERNAME
+  administrator_login_password = var.DB_PASSWORD
 
   tags = {
     environment = "production"
   }
 }
 
-resource "azurerm_sql_server" "heroes_db" {
-  name                         = "HeroesDb"
-  resource_group_name          = azurerm_resource_group.db_rg.name
-  location                     = azurerm_resource_group.db_rg.location
-  version                      = "12.0"
-  administrator_login          = var.username
-  administrator_login_password = var.password
-
-  tags = {
-    environment = "production"
-  }
+resource "azurerm_mssql_database" "users_db" {
+  name           = "josh-users-db-123"
+  server_id      = azurerm_sql_server.users_db_server.id
+  sku_name       = "Basic"
 }
