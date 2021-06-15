cb8a76bcb81adea2d2e5b53f2d9821722417ca65
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-06-15 16:47:44 +0100

    Add an access token generator for session tokens

---
+++ WarOfHeroesAPI/Users/AccessTokenGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WarOfHeroesUsersAPI.Users
+{
+    public static class AccessTokenGenerator
+    {
+        public static string GenerateAccessToken()
+        {
+            var guid = Guid.NewGuid();
+            var currentDate = $"{DateTime.UtcNow:yyyyMMdd}";
+
+            return $"WoH-{currentDate}-{guid}";
+        }
+    }
+}
