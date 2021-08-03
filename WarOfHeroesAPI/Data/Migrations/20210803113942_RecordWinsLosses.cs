0f16b8fc554f12b787b87486d43189ac37aac9f8
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-08-03 12:40:43 +0100

    Add DB migrations

---
+++ WarOfHeroesAPI/Data/Migrations/20210803113942_RecordWinsLosses.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfHeroesUsersAPI.Migrations
+{
+    public partial class RecordWinsLosses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Coins",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Losses",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Wins",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Coins",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Losses",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Wins",
+                table: "Users");
+        }
+    }
+}
